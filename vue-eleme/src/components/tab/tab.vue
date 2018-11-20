fac21fc34fcf55ff37d197eb42a08172814e7909
jiangxin <jiangxin0211@163.com>
2018-11-20 18:25:15 +0800

    cube-scroll-nav&cube-scroll-nav-panel实现左右联动

--- vue-eleme/src/components/tab/tab.vue
+++ vue-eleme/src/components/tab/tab.vue
@@ -20,7 +20,7 @@
         :options="slideOptions"
       >
         <cube-slide-item v-for="(tab, index) in tabs" :key="index">
-          <component :is="tab.component" :data="tab.data"></component>
+          <component :is="tab.component" :data="tab.data" ref="component"></component>
         </cube-slide-item>
       </cube-slide>
     </div>
@@ -34,7 +34,7 @@ export default {
     tabs: {
       type: Array,
       default () {
-        return {}
+        return []
       }
     },
     initialIndex: {
@@ -66,9 +66,14 @@ export default {
       }
     }
   },
+  mounted () {
+    this.onChange(this.index) // 进入当前页面的组件就加载数据
+  },
   methods: {
     onChange (current) {
       this.index = current
+      const component = this.$refs.component[current]
+      component.fetch && component.fetch()  // 滑入组件的时候才加载当前组件的数据
     },
     onScorll (pos) {
       const tabBarWidth = this.$refs.tabBar.$el.clientWidth
